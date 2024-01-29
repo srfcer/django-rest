7ea3999a4ebce54953b597c6fa62bcf36cf6ad28
Surf <jcevra@hotmail.com>
2024-01-28 20:28:30 -0500

    Registro de usuario sin Token

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
@@ -18,7 +18,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         if password!= password2:
             raise serializers.ValidationError({'error': 'El password no coincide'})
         
-        if User.objects.filter(username=self.validated_data['email'].exists()).exists():
+        if User.objects.filter(email=self.validated_data['email']).exists():
             raise serializers.ValidationError({'error': 'Ya existe un usuario con ese email'})
         
         account = User(email=self.validated_data['email'], username=self.validated_data['username'])
