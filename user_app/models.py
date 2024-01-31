41cefefa4d2269e4ab2254ca12fd256734e3b303
Julio Surf <jcevra@hotmail.com>
2024-01-30 21:50:00 -0500

    correccion models de user_app

--- user_app/models.py
+++ user_app/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 # Create your models here.
-@@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
