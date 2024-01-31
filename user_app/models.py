4ff3415bbcd47f6071d94d29d2463987adf4b164
Julio Surf <jcevra@hotmail.com>
2024-01-30 22:19:22 -0500

    view y models de user_app

--- user_app/models.py
+++ user_app/models.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework import Token
-from django.db import models
-
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
